7b177748dfbe71dbc51f53b71c1e2d05bd064223
SujanBallari <109755947+SujanBallari@users.noreply.github.com>
2022-10-18 15:30:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
+Hello.....
+adding this branch in master branch
 adding this line in dev branch
-this file contains some deployment script
+
+this file contains some deployment script..
+
+I am updating this file in stage branch..
